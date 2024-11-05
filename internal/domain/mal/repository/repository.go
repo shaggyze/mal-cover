cb68cde6701f0449c6bb06a9886a7d57cc96a4df
shaggyze <shaggyze@gmail.com>
2024-11-05 15:44:33 -0800

    Bug fixes

--- internal/domain/mal/repository/repository.go
+++ internal/domain/mal/repository/repository.go
@@ -8,5 +8,5 @@ import (
 
 // Repository contains functions for mal domain.
 type Repository interface {
-	GetList(ctx context.Context, username string, mainType string, status int, genre int) ([]entity.Entry, int, error)
+	GetList(ctx context.Context, username string, mainType string, status string, genre string) ([]entity.Entry, int, error)
 }
